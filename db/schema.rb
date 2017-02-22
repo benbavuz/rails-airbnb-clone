123a5435bf77b1668beb876c6ea656007d0765f8
Salim Benfarhat <s.benfarhat@my.com>
2017-02-22 13:18:48 +0100

    installing money-rails for the price and display on index and show lawyers views

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170221173248) do
+ActiveRecord::Schema.define(version: 20170222120629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +35,10 @@ ActiveRecord::Schema.define(version: 20170221173248) do
     t.string   "last_name"
     t.string   "phone_number"
     t.string   "email"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "user_id"
+    t.integer  "price_cents",  default: 0, null: false
     t.index ["user_id"], name: "index_lawyers_on_user_id", using: :btree
   end
 
@@ -58,7 +58,9 @@ ActiveRecord::Schema.define(version: 20170221173248) do
     t.string   "phone_number"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "lawyer_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["lawyer_id"], name: "index_users_on_lawyer_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
