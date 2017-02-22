75227f076bb063409d5d56eba9e831c88a912ea6
Salim Benfarhat <s.benfarhat@my.com>
2017-02-22 18:11:46 +0100

    juste adding the route for profil page and linkink the profil to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => '/attachinary'
   resources :users, only: [] do
     get '/lawyers', to: "lawyers#index_by_user"
+    get '/profile', to: "pages#profile"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
