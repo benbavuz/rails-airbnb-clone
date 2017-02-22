47129971979810ba2015a5eebbe282f2838e73d8
Fabien Martinez <fab.mrtnz22@gmail.com>
2017-02-22 18:23:29 +0100

    conflits 2 solves

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :lawyers, only: [:index, :show, :create, :new, :edit, :update, :destroy]
   mount Attachinary::Engine => '/attachinary'
   resources :users, only: [] do
-    get '/lawyers', to: "lawyers#index_by_user" do
-      resources :users, only: [:new, :create]
+    get '/lawyers', to: "lawyers#index_by_user"
+    get '/profile', to: "pages#profile"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ Lawyer.destroy_all
 LawyerSpeciality.destroy_all
 Speciality.destroy_all
 
-lawyer2 = Lawyer.create(first_name:"Salim", last_name:"lilim", phone_number: "07798274834", email:"salim@gmail.com")
-lawyer3 = Lawyer.create(first_name:"SalimB", last_name:"benfarat", phone_number: "0667877898", email:"salim@yahoo.com")
-lawyer4 = Lawyer.create(first_name:"fabien", last_name:"frmz", phone_number: "0889787678", email:"fabien@gmail.com")
+lawyer2 = Lawyer.create(first_name:"Saliim", last_name:"lilim", phone_number: "07798274834", email:"salim@gmail.com", address:"7 bd des chenes Marseille", user_id:1)
+lawyer3 = Lawyer.create(first_name:"SaliimB", last_name:"benfarat", phone_number: "0667877898", email:"salim@yahoo.com", address:"10 rue paradis marseille", user_id:1)
+lawyer4 = Lawyer.create(first_name:"fabiien", last_name:"frmz", phone_number: "0889787678", email:"fabien@gmail.com", address:"gare saint charles marseille", user_id:1)
 
 specialities = ["Admiralty and Maritime Law", "Appellate Law", "Bankruptcy Law", "Criminal Law", "Estate Planning, Trust and Probate Law", "Family Law", "Franchise and Distribution Law", "Immigration and Nationality Law", "Legal Malpractice Law", "Taxation Law", "Workers' Compensation Law"]
 
