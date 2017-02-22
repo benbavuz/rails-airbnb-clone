02f51c582090da8dea162e75ca38a62791d938b3
Salim Benfarhat <s.benfarhat@my.com>
2017-02-23 00:34:30 +0100

    linking appointment view to the subnavigation link in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [] do
     get '/lawyers', to: "lawyers#index_by_user"
     get '/profile', to: "pages#profile"
+    get '/appointments', to: "pages#appointments"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
