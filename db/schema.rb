861d711efe045536718ac8d2172477146cb512e4
Salim Benfarhat <s.benfarhat@my.com>
2017-02-23 12:30:19 +0100

    finishing fb login integration and adding style on button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222120629) do
+ActiveRecord::Schema.define(version: 20170223103007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,10 @@ ActiveRecord::Schema.define(version: 20170222120629) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "user_id"
-    t.integer  "price_cents",  default: 0, null: false
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "price_cents",  default: 0, null: false
     t.index ["user_id"], name: "index_lawyers_on_user_id", using: :btree
   end
 
@@ -76,6 +76,11 @@ ActiveRecord::Schema.define(version: 20170222120629) do
     t.string   "phone_number"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
